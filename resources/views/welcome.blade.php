4cd7ae6578259eefbcb1c7fd296bd594a3f1a960
hare <washi79washi@gmail.com>
2023-12-16 16:42:01 +0900

    feat: 日本語化Laravel Breeze導入

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,37 @@
         
     </head>
     <body class="">
+        <header class="bg-success text-dark bg-opacity-10">
+            <div class="container d-flex justify-content-between align-items-center">
+                {{-- <a href="{{ route('post.index') }}" class="d-flex justify-content-between align-items-center">
+                    <img src="{{ asset('friends.png') }}" alt="みんなの掲示板のロゴ" width="70">
+                    <h1 class="fs-4 fw-bold">みんなの<span class="header__title"></span>掲示板</h1>
+                </a> --}}
+                <nav class="navbar">
+                    <ul class="d-flex">
+                        @auth
+                        <li class="nav-item"><a class="nav-link active text-dark p-2" aria-current="page"
+                                href="{{ route('profile.edit') }}">アカウント設定</a></li>
+                        <li class="nav-item">
+                            <form method="POST" action="{{ route('logout') }}" class="nav-link p-2">
+                                @csrf
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">ログアウト</a>
+                                </form>
+                            </li>
+                            @endauth
+                            @guest
+                        <li class="nav-item">
+                            <a class="nav-link text-dark p-2" href="{{ route('register') }}">ユーザー登録</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark p-2" href="{{ route('login') }}">ログイン</a>
+                        </li>
+                        @endguest
+                    </ul>
+                </nav>
+            </div>
+        </header>
         <h1 class="text-bg-secondary">こんにちは</h1>
     </body>
 </html>
