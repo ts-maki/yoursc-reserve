b1b7d83f5d25c97ca6660b6f9aba4b9df53150a9
hare <washi79washi@gmail.com>
2023-12-18 20:50:25 +0900

    feat: 問い合わせステータスによって背景色を変える

--- resources/views/admin/inquiry/index.blade.php
+++ resources/views/admin/inquiry/index.blade.php
@@ -16,16 +16,34 @@
             </thead>
             <tbody>
                 @foreach ($inquiries as $index => $inquiry)
-                    <tr onclick="window.location.href='{{ route('admin.inquiry.show', $inquiry->id) }}'" class="cursor-pointer">
-                        <th>{{ $index + 1 }}</th>
-                        <th>{{ $inquiry->inquiryStatus->status }}</th>
-                        <th>{{ $inquiry->inquiryType->name }}</th>
-                        <th>{{ $inquiry->first_name }} {{ $inquiry->last_name }}</th>
-                        <th>{{ $inquiry->email }}</th>
-                        <th>{{ $inquiry->telephone_number }}</th>
-                        <th>{{ $inquiry->stay_date }}</th>
-                        <th>{{ $inquiry->message }}</th>
-                    </tr>
+                @php
+                switch ($inquiry->inquiry_status_id) {
+                case 1:
+                $background_color = 'bg-danger';
+                break;
+                case 2:
+                $background_color = 'bg-primary';
+                break;
+                case 3:
+                $background_color = 'bg-success';
+                break;
+                default:
+                $background_color = 'bg-danger';
+                break;
+                }
+                @endphp
+                <tr onclick="window.location.href='{{ route('admin.inquiry.show', $inquiry->id) }}'"
+                    class="cursor-pointer">
+                    <th>{{ $index + 1 }}</th>
+                    <th><span class="border rounded p-1 bg-opacity-50 {{ $background_color }}">{{
+                            $inquiry->inquiryStatus->status }}</span></th>
+                    <th>{{ $inquiry->inquiryType->name }}</th>
+                    <th>{{ $inquiry->first_name }} {{ $inquiry->last_name }}</th>
+                    <th>{{ $inquiry->email }}</th>
+                    <th>{{ $inquiry->telephone_number }}</th>
+                    <th>{{ $inquiry->stay_date }}</th>
+                    <th>{{ $inquiry->message }}</th>
+                </tr>
                 @endforeach
             </tbody>
         </table>
