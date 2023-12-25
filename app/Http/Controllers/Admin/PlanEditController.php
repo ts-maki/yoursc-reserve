557d475152791a3d9c5cc6f0b0c921845d45e85e
hare <washi79washi@gmail.com>
2023-12-25 13:16:03 +0900

    feat: プランの差し替え画像がプランに登録なければ登録

--- app/Http/Controllers/Admin/PlanEditController.php
+++ app/Http/Controllers/Admin/PlanEditController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Image;
 use App\Models\Plan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use PhpParser\Node\Stmt\TryCatch;
 
 class PlanEditController extends Controller
 {
@@ -17,17 +20,25 @@ class PlanEditController extends Controller
 
     public function update(Request $request, $plan_id)
     {
+
+        $plan = Plan::findOrFail($plan_id);
+
+        $plan->title = $request->title;
+        $plan->description = $request->description;
+        $plan->save();
+
         //既存の画像更新
-        // dd($request->image);
         if ($request->hasFile('image')) {
             foreach ($request->file('image') as $index => $file) {
                 $upload_file_name = $file->getClientOriginalName();
                 $existed_file = Image::where('id', $index)->first();
                 $existed_file_name = str_replace('storage/images/', '', $existed_file->path);
-                // dd($existed_file_name);
-                $file_name = 'storage/images/'. $upload_file_name;
-                //ファイル名が既存と異なる場合更新
-                if ($upload_file_name !== $existed_file_name) {
+                $file_name = 'storage/images/' . $upload_file_name;
+
+                //差し替えた画像が宿泊プランに既に登録されている場合登録しない
+                $is_file_name = Image::where('plan_id', $plan_id)->where('path', $file_name)->exists();
+                Log::debug("差し替えファイル名のプランの登録可否", [$is_file_name]);
+                if (!$is_file_name) {
                     $path = $file->storeAs('images', $upload_file_name, 'public');
                     $existed_file->path = $file_name;
                     $existed_file->save();
@@ -39,12 +50,12 @@ class PlanEditController extends Controller
         if ($request->hasFile('image_plus')) {
             foreach ($request->file('image_plus') as $file) {
                 $upload_file_name = $file->getClientOriginalName();
-                $file_path = 'storage/images/'. $upload_file_name;
+                $file_path = 'storage/images/' . $upload_file_name;
                 $path = $file->storeAs('images', $upload_file_name, 'public');
 
                 //ファイル名が宿泊プランと紐づくものと異なる場合登録
                 $is_file_name = Image::where('plan_id', $plan_id)->where('path', $file_path)->exists();
-                // dd($is_file_name);
+                Log::debug("追加ファイル名のプランの登録可否", [$is_file_name]);
                 if (!$is_file_name) {
                     Image::create([
                         'plan_id' => $plan_id,
