b781f29b5ce3d2512ca053eb88738a38881d3032
hare <washi79washi@gmail.com>
2023-12-29 11:44:16 +0900

    feat: プラン一覧をプランに登録されている一番早い予約枠の日付順に並び替える

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -12,9 +12,23 @@ class PlanController extends Controller
     {
         // TODO config('global.cache.time');が読み込めない
         // dd(config('global.cache.time'));
-        $plans = Cache::remember('plans', 5, function () {
-            return Plan::with('images:plan_id,path')->select('id', 'title', 'description')->get();
+        //画像のEagerロードと日付順に並び替えた関連先の予約枠を取得
+        $plans = Cache::remember('plans', 1, function () {
+            return Plan::with(['images:plan_id,path', 'planReserveSlots.reserveSlot' => function($query) {
+                return $query->orderBy('date');
+            } ])->get();
         });
+
+        /* プラン一覧をプランに登録されている一番早い日付の予約枠の日付順に並び替える
+        比較対象のプランのプランと予約枠の関係を予約枠が一番早い順に並び替える
+        →並び替えたレコードのはじめの日付を取得する
+        */
+        $plans = $plans->sortBy(function ($plan) {
+            return $plan->planReserveSlots->sortBy(function ($reserve_slot_date) {
+                return $reserve_slot_date->reserveSlot->date;
+            })->first()->reserveSlot->date;
+        });
+
         return view('plan.index')->with('plans', $plans);
     }
 
--- resources/views/plan/show.blade.php
+++ resources/views/plan/show.blade.php
@@ -1,20 +1,11 @@
 <x-layout>
     <x-container>
         <h2 class="fs-2">{{ $plan->title }}</h2>
-        <div class="row">
-            <div class="col mt-4">
-                <div class="card">
-                    <div class="row">
-                        @foreach ($plan->images as $image)
-                            <img src="{{ asset($image->path) }}" alt="{{ $plan->title }}プランの画像" class="col" style="width: 50px">
-                        @endforeach
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $plan->title }}</h5>
-                        <p class="card-text">{{ $plan->description }}</p>
-                    </div>
-                </div>
-            </div>
+        <div class="d-flex">
+            @foreach ($plan->images as $image)
+            <img src="{{ asset($image->path) }}" alt="{{ $plan->title }}プランの画像" width="200">
+            @endforeach
         </div>
+            <p>{{ $plan->description }}</p>
     </x-container>
 </x-layout>
