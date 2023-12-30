7d0958e5811056167b1f6ae35081abfd8ce69066
hare <washi79washi@gmail.com>
2023-12-30 11:52:40 +0900

    feat: フルカレンダー初期化コード記述

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,8 @@
     <meta name="description" content="宿泊予約サイト" />
     <meta name="format-detection" content="telephone=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{-- <link rel="icon" href="{{ asset('/favicon.ico') }}" /> --}}
+    {{--
+    <link rel="icon" href="{{ asset('/favicon.ico') }}" /> --}}
     <link rel="apple-touch-icon" href="{{ asset('/favicon.ico') }}" />
     <meta property="og:site_name" content="{{ config('app.name', '宿泊予約サイト') }}" />
     {{-- TODO 本番環境のURLにする --}}
@@ -17,13 +18,27 @@
     <meta property="og:description" content="宿泊予約ができるサイト" />
     <meta property="og:locale" content="ja_JP" />
     {{-- TODO 本番環境のシェア用画像のURLを絶対パスで指定する --}}
-    {{-- <meta property="og:image" content="{{ asset('friends.png') }}" /> --}}
+    {{--
+    <meta property="og:image" content="{{ asset('friends.png') }}" /> --}}
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:description" content="みんなが投稿できる掲示板" />
     {{-- TODO Twitter専用の記述です。シェアされた時に表示したい画像を絶対パスで設定 --}}
-    {{-- <meta name="twitter:image:src" content="{{ asset('friends.png') }}" /> --}}
+    {{--
+    <meta name="twitter:image:src" content="{{ asset('friends.png') }}" /> --}}
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css',])
     <title>{{ config('app.name', '宿泊予約サイト') }}</title>
+
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
 </head>
 
 <body>
