19b73dc2b634323c23ed75f4a65e8a6a2dc3e40d
hare <washi79washi@gmail.com>
2024-01-03 19:43:10 +0900

    feat: githubページへの案内を追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-container>
         @if (session('complete_inquiry'))
-            <p>{{ session('complete_inquiry') }}</p>
+        <p>{{ session('complete_inquiry') }}</p>
         @endif
         <div class="mt-2">
             <a href="{{ route('access.index') }}" class="btn btn-outline-primary">アクセス案内</a>
@@ -17,6 +17,12 @@
         </div>
         <div class="mt-4">
             <a href="{{ route('login') }}" class="btn btn-outline-dark">管理者ページへ</a>
+        </div>
+        <div class="mt-4">
+            <p>サイトとコードについてgithubにまとめています。</p>
+            <a href="https://github.com/mirai-79/yoursc-reserve"><img src="{{ asset('/images/github-mark.svg') }}"
+                    alt="githubのロゴ" width="50" class="mt-2"></a>
+            <a href="https://github.com/mirai-79/yoursc-reserve" class="btn btn-outline-success mt-2">githubページへ</a>
         </div>
     </x-container>
 </x-layout>
