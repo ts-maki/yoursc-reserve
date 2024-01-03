7c4f0a69e1d3fa6b2984ab5fa4ec57f1ec467d6d
hare <washi79washi@gmail.com>
2024-01-03 15:51:24 +0900

    feat: プラン削除したら予約に紐づく予約枠の部屋の数を1足す

--- app/Http/Controllers/Admin/PlanDeleteController.php
+++ app/Http/Controllers/Admin/PlanDeleteController.php
@@ -7,6 +7,8 @@ use App\Models\Image;
 use Illuminate\Support\Str;
 
 use App\Models\Plan;
+use App\Models\Reserve_slot;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
@@ -20,32 +22,47 @@ class PlanDeleteController extends Controller
 
     public function destroy($plan_id)
     {
-        $plan = Plan::findOrFail($plan_id);
+        DB::transaction(function () use ($plan_id) {
+            $plan = Plan::findOrFail($plan_id);
 
-        //宿泊プランと部屋の関連解除
-        $plan->planRoom()->detach();
-        
-        //宿泊プランと予約枠の関連解除
-        $plan->planReserveSlot()->detach();
+            //宿泊プランと予約枠の関連解除
+            $plan->planReserveSlot()->detach();
 
-        //宿泊プランに紐づく画像path削除
-        $file_paths = $plan->images()->pluck('path');
-        $plan->images()->delete();
+            //宿泊プランに紐づく画像path削除
+            $file_paths = $plan->images()->pluck('path');
+            $plan->images()->delete();
 
-        //他の宿泊プランで使用されていない画像をディスクから削除
-        foreach ($file_paths as $file_path) {
-            $is_file = Image::where('path', $file_path)->exists();
-            Log::debug("他の宿泊プランで使用されてるか", [$is_file]);
-            if (!$is_file) {
-                $file = Str::remove('storage/', $file_path);
-                if (Storage::disk('public')->exists($file)) {
-                    Log::debug("ディスクにあり", [$file]);
-                    Storage::disk('public')->delete($file);
+            //他の宿泊プランで使用されていない画像をディスクから削除
+            foreach ($file_paths as $file_path) {
+                $is_file = Image::where('path', $file_path)->exists();
+                Log::debug("他の宿泊プランで使用されてるか", [$is_file]);
+                if (!$is_file) {
+                    $file = Str::remove('storage/', $file_path);
+                    if (Storage::disk('public')->exists($file)) {
+                        Log::debug("ディスクにあり", [$file]);
+                        Storage::disk('public')->delete($file);
+                    }
                 }
             }
-        }
 
-        $plan->delete();
+            //プランに紐づく予約の予約枠の部屋数を1足す
+            $plan->reserves->map(function ($reserve) {
+                // dd($reserve->reserve_slot_id);
+                $reserve_slot = Reserve_slot::findOrFail($reserve->reserve_slot_id);
+                $reserve_slot->number_of_rooms += 1;
+                Log::debug('プラン消した際に予約枠の部屋の数を1足す', [$reserve_slot]);
+                $reserve_slot->save();
+    
+                return $reserve;
+            });
+
+            //宿泊プランに紐づく予約との関連解除
+            $plan->reservePlan()->detach();
+  
+            $plan->delete();
+
+        });
+
         return to_route('admin.plan.index');
     }
 }
